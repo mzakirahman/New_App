1f7d6bbe81b184890a4ab78b5af482c3deb9a823
mzakirahman <mzakiriau@gmail.com>
2021-04-26 23:25:43 +0700

    membuat file search_viuw.dart dalam folder viuws

---
+++ lib/views/search_viuw.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SearchVoiw extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("SearchVoiw"),
+      )
+    );
+  }
+}
