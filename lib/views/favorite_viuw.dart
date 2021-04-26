d14d02a9d77ed6a99e420f0dd9bcbaa9f18f4bb8
mzakirahman <mzakiriau@gmail.com>
2021-04-26 23:29:49 +0700

    membuat file favorite_viuw.dart dalam folder views

--- lib/views/favorite_viuw.dart
+++ lib/views/favorite_viuw.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class SearchVoiw extends StatelessWidget {
+class FavoriteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("SearchVoiw"),
+        child: Text("FavoriteView"),
       )
     );
   }
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class HomeViuw extends StatelessWidget {
+class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("HomeViuw"),
+        child: Text("SearchView"),
       )
     );
   }
